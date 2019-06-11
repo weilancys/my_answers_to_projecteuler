b5ff5770c7ff59842741715832b5c3852084eaaa
lightblue <weilancys@hotmail.com>
2019-06-12 00:29:17 +0800

    still not finished problem 10.

--- python/problem_10.py
+++ python/problem_10.py
@@ -5,19 +5,40 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 """
 
+import threading
+from queue import Queue
+
+sum = 0
+
+def make_queue():
+    queue = Queue()
+    for i in range(2, 2000001):
+        queue.put(i)
+    return queue
+
 def is_prime_number(number):
     if number == 2:
         return True
-    for i in range(2, number):
+    for i in range(3, number, 2):
         if number % i == 0:
             return False
     return True
 
+def thread_worker(number, lock):
+    global sum
+    if is_prime_number(number):
+        with lock:
+            sum += number
+
+
 def find_sum_of_all_primes_below_two_million():
-    sum = 0
-    for i in range(2, 2000001):
-        if is_prime_number(i):
-            sum += i 
+    queue = make_queue()
+    lock = threading.Lock()
+
+    while not queue.empty():
+        t = threading.Thread(target=thread_worker, args=(queue.get(), lock))
+        t.start()
+
     return sum
 
 if __name__ == "__main__":
